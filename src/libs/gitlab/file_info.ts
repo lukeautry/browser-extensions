da0d6536994d3f51a3493349e9656159b5276eb3
Chris Wendt <chrismwendt@gmail.com>
2018-10-24 11:51:49 -0700

    fix: call resolveRev when getting file info

--- src/libs/gitlab/file_info.ts
+++ src/libs/gitlab/file_info.ts
@@ -1,8 +1,8 @@
 import { Observable, of, throwError, zip } from 'rxjs'
 import { map, switchMap } from 'rxjs/operators'
 
-import { resolveRev, retryWhenCloneInProgressError } from '../../shared/repo/backend'
 import { FileInfo } from '../code_intelligence'
+import { ensureRevisionsAreCloned } from '../code_intelligence/utils/file_info'
 import { getBaseCommitIDForCommit, getBaseCommitIDForMergeRequest } from './api'
 import {
     getCommitIDFromPermalink,
@@ -12,19 +12,6 @@ import {
     getFilePathsFromCodeView,
     getHeadCommitIDFromCodeView,
 } from './scrape'
-
-const ensureRevisionsAreCloned = (files: Observable<FileInfo>): Observable<FileInfo> =>
-    files.pipe(
-        switchMap(({ repoPath, rev, baseRev, ...rest }) => {
-            // Although we get the commit SHA's from elesewhere, we still need to
-            // use `resolveRev` otherwise we can't guarantee Sourcegraph has the
-            // revision cloned.
-            const resolvingHeadRev = resolveRev({ repoPath, rev }).pipe(retryWhenCloneInProgressError())
-            const resolvingBaseRev = resolveRev({ repoPath, rev: baseRev }).pipe(retryWhenCloneInProgressError())
-
-            return zip(resolvingHeadRev, resolvingBaseRev).pipe(map(() => ({ repoPath, rev, baseRev, ...rest })))
-        })
-    )
 
 /**
  * Resolves file information for a page with a single file, not including diffs with only one file.
@@ -49,7 +36,7 @@ export const resolveFileInfo = (): Observable<FileInfo> => {
             filePath,
             commitID,
             rev,
-        })
+        }).pipe(ensureRevisionsAreCloned)
     } catch (error) {
         return throwError(error)
     }
